4a632422d0738ceb2df70e12dc99fb79988128b5
Julian Hoffmann <julian.hoffmann@uni-a.de>
2023-10-24 14:27:00 +0200

    updated design

--- FiletypeChooser.py
+++ FiletypeChooser.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import sv_ttk as sv
+from tkinter import ttk
 from tkinter import filedialog
 from filetypes import FileTypes
 from genericconverter import GenericConverter
@@ -6,24 +8,36 @@ from genericconverter import GenericConverter
 root = tk.Tk()
 root.title('Image Converter')
 
+sv.set_theme("dark")
+
 root.update()
-root.minsize(root.winfo_width() + 75, root.winfo_height())
+root.minsize(root.winfo_width() + 100, root.winfo_height() - 35)
 x_cordinate = int((root.winfo_screenwidth() / 2) - (root.winfo_width() / 2))
 y_cordinate = int((root.winfo_screenheight() / 2) - (root.winfo_height() / 2))
 root.geometry("+{}+{}".format(x_cordinate, y_cordinate-20))
+root.resizable(False, False)
 
-current_from = tk.StringVar(value=FileTypes.HEIC.value)
+current_from = tk.StringVar()
+default_from = FileTypes.HEIC.value
 options_from = [option.value for option in FileTypes]
 options_from.sort()
-tk.Label(root, text='Select a file type to convert').pack()
-tk.OptionMenu(root, current_from, *options_from).pack()
 
-current_to = tk.StringVar(value=FileTypes.PNG.value)
+label_from = ttk.Label(root, text='File type to convert from:')
+label_from.grid(row=0, column=0, pady=15, padx=10)
+optionmenu_from = ttk.OptionMenu(root, current_from, default_from, *options_from)
+optionmenu_from.grid(row=0, column=1, padx=20)
+
+current_to = tk.StringVar()
+default_to = FileTypes.PNG.value
 options_to = [option.value for option in FileTypes if option.value not in ["JPG", "HEIC", "SVG"]]
 options_to.sort()
-tk.Label(root, text='Select a file type to convert to').pack()
-tk.OptionMenu(root, current_to, *options_to).pack()
 
-tk.Button(root, text='Convert', command=lambda: GenericConverter.generic_converter(GenericConverter, current_from.get(), current_to.get(), root)).pack()
+label_to = ttk.Label(root, text='File type to convert to:')
+label_to.grid(row=1, column=0, pady=15, padx=10)
+optionmenu_to = ttk.OptionMenu(root, current_to, default_to, *options_to)
+optionmenu_to.grid(row=1, column=1, padx=20)
+
+button_convert = ttk.Button(root, text='Convert', command=lambda: GenericConverter.generic_converter(GenericConverter, current_from.get(), current_to.get(), root))
+button_convert.grid(row=2, column=0, columnspan=2, pady=10)
 
 root.mainloop()
