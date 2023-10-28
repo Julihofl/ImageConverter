2259b26f1ada2ee218090a46bbf0c8e8aea5a628
Julian <julian.hoffmann@uni-a.de>
2023-10-29 00:51:54 +0200

    renamed main file and added placeholder help button

--- imageconverter.py
+++ imageconverter.py
@@ -18,15 +18,18 @@ def main():
     root.geometry("+{}+{}".format(x_cordinate, y_cordinate-20))
     root.resizable(False, False)
 
+    button_help = ttk.Button(root, text='Help')
+    button_help.grid(row=0, column=0, columnspan=2, pady=10, padx=10, sticky='nw')
+
     current_from = tk.StringVar()
     default_from = FileTypes.HEIC.value
     options_from = [option.value for option in FileTypes]
     options_from.sort()
 
     label_from = ttk.Label(root, text='File type to convert from:')
-    label_from.grid(row=0, column=0, pady=15, padx=10)
+    label_from.grid(row=1, column=0, pady=15, padx=10)
     optionmenu_from = ttk.OptionMenu(root, current_from, default_from, *options_from)
-    optionmenu_from.grid(row=0, column=1, padx=20)
+    optionmenu_from.grid(row=1, column=1, padx=20)
 
     current_to = tk.StringVar()
     default_to = FileTypes.PNG.value
@@ -34,12 +37,12 @@ def main():
     options_to.sort()
 
     label_to = ttk.Label(root, text='File type to convert to:')
-    label_to.grid(row=1, column=0, pady=15, padx=10)
+    label_to.grid(row=2, column=0, pady=15, padx=10)
     optionmenu_to = ttk.OptionMenu(root, current_to, default_to, *options_to)
-    optionmenu_to.grid(row=1, column=1, padx=20)
+    optionmenu_to.grid(row=2, column=1, padx=20)
 
-    button_convert = ttk.Button(root, text='Convert', command=lambda: GenericConverter.generic_converter(GenericConverter, current_from.get(), current_to.get(), root))
-    button_convert.grid(row=2, column=0, columnspan=2, pady=10)
+    button_convert = ttk.Button(root, text='Choose files', command=lambda: GenericConverter.generic_converter(GenericConverter, current_from.get(), current_to.get(), root))
+    button_convert.grid(row=3, column=0, columnspan=2, pady=10)
 
     root.mainloop()
 
