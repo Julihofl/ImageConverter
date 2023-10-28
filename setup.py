dbed7e2520df4ab7db48cdf2729463fcc60e95d0
Julian <julian.hoffmann@uni-a.de>
2023-10-28 12:55:45 +0200

    using cx_freeze instead of pyinstaller

---
+++ setup.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+
+base = "Win32GUI"    
+
+include_files = [("images", "images")]
+
+executables = [Executable("FiletypeChooser.py", base=base)]
+
+packages = ["idna", "tkinter", "sv_ttk", "filetypes", "genericconverter", "pillow_heif", "PIL", "pdf2image", "wand", "io"]
+options = {
+    'build_exe': {    
+        'packages':packages,
+    },    
+}
+
+setup(
+    name = "Image Converter",
+    options = options,
+    version = "1.0",
+    description = 'Image Converter by Julian',
+    executables = executables
+)
