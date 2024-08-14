a65950229a02a713e33828fe4e33d906633a66c2
Julian Hoffmann <julian.hoffmann@outlook.com>
2024-08-15 01:21:56 +0200

    more refactoring

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ import io
 import os
 import wand.color
 import wand.image
+import tkinter as tk
 
 from tkinter import filedialog
 from pdf2image import convert_from_path
@@ -9,68 +10,70 @@ from wand.api import library
 from PIL import Image
 from pillow_heif import register_heif_opener
 from filetypes import Filetypes
+from typing import List
 
 register_heif_opener()
 
 class Converter:
-    def __init__(self, root, target):
-        self.root = root
-        self.target = target.upper()
-        self.extension = f'.{self.target.lower()}'
-        self.valid_extensions = [filetype.value.lower() for filetype in Filetypes]
+    def __init__(self, root: tk.Tk, target: str) -> None:
+        self.root: tk.Tk = root
+        self.target: str = target.upper()
+        self.old_extension: str  = f'.{self.target.lower()}'
+        self.new_extension: str = ''
+        self.valid_extensions: List[str] = [filetype.value.lower() for filetype in Filetypes]
 
-    def convert(self):
-        images_path = filedialog.askopenfilenames(defaultextension=self.extension, filetypes=[("All files", "*.*")], title='Select images to convert')
+    def convert(self) -> None:
+        images_paths: List[str] = filedialog.askopenfilenames(filetypes=[("All files", "*.*")], title='Select images to convert')
 
-        for image_path in images_path:
-            extension = os.path.splitext(image_path)[1][1:].lower()
-            if extension not in self.valid_extensions:
-                print(f"Unsupported file type: {extension}")
+        for image_path in images_paths:
+            self.new_extension: str = os.path.splitext(image_path)[1][1:].lower()
+            if self.new_extension not in self.valid_extensions:
+                print(f"Unsupported file type: {self.new_extension}")
                 continue
 
             try:
-                self._convert_file(image_path, extension)
+                self._convert_file(image_path)
             except Exception as e:
                 print(f"Fehler bei der Verarbeitung von {image_path}: {e}")
         
         self.root.destroy()
 
-    def _convert_file(self, image_path, extension):
+    def _convert_file(self, image_path: str) -> None:
         if self.target == 'WEBP':
-            self._convert_webp(image_path, extension)
+            self._convert_webp(image_path)
         elif self.target == 'SVG':
-            self._convert_svg(image_path, extension)
+            self._convert_svg(image_path)
         elif self.target == 'PDF':
-            self._convert_pdf(image_path, extension)
+            self._convert_pdf(image_path)
         else:
-            self._convert_image(image_path, extension)
+            self._convert_image(image_path)
 
-    def _convert_webp(self, image_path, extension):
-        image = Image.open(image_path).convert('RGB')
-        self._save_image(image, image_path, extension)
+    def _convert_webp(self, image_path: str) -> None:
+        image: Image = Image.open(image_path).convert('RGB')
+        self._save_image(image, image_path)
 
-    def _convert_svg(self, image_path, extension):
+    def _convert_svg(self, image_path: str) -> None:
         with io.open(image_path, "rb") as svg_file:
             with wand.image.Image() as image:
                 with wand.color.Color('transparent') as background_color:
                     library.MagickSetBackgroundColor(image.wand, background_color.resource)
                 image.read(blob=svg_file.read(), format="svg")
-                png_image = image.make_blob("png32")
+                png_image: bytes = image.make_blob("png32")
         
-        image_path = image_path.replace(extension, self.extension)
+        image_path = image_path.replace(self.new_extension, self.old_extension)
         with open(image_path, "wb") as out:
             out.write(png_image)
 
-    def _convert_pdf(self, image_path, extension):
+    def _convert_pdf(self, image_path: str) -> None:
         pages = convert_from_path(image_path)
         for idx, page in enumerate(pages):
-            temp_path = f"{image_path.removesuffix(extension)}_{idx + 1}{self.extension}"
+            temp_path = f"{image_path.removesuffix(self.new_extension)}_{idx + 1}{self.old_extension}"
             page.save(temp_path, self.target)
 
-    def _convert_image(self, image_path, extension):
-        image = Image.open(image_path)
-        self._save_image(image, image_path, extension)
+    def _convert_image(self, image_path: str) -> None:
+        image: Image = Image.open(image_path)
+        self._save_image(image, image_path)
 
-    def _save_image(self, image, image_path, extension):
-        image_path = image_path.replace(extension, self.extension)
+    def _save_image(self, image: Image, image_path: str) -> None:
+        image_path = image_path.replace(self.new_extension, self.old_extension)
         image.save(image_path, format=self.target)
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from filetypes import Filetypes
 from converter import Converter
 
 class ImageConverterApp:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, root: tk.Tk):
+        self.root: tk.Tk = root
         self.root.title('Image Converter')
         self.root.iconbitmap('images\\icon.ico')
 
@@ -18,41 +18,37 @@ class ImageConverterApp:
         self.create_widgets()
         root.mainloop()
 
-    def center_window(self, window, width_offset=0, height_offset=0):
+    def center_window(self, window: tk.Tk, width_offset: int = 0, height_offset: int = 0) -> None:
         window.update()
         window.minsize(window.winfo_width() + width_offset, window.winfo_height() + height_offset)
-        x_cordinate = int((window.winfo_screenwidth() / 2) - (window.winfo_width() / 2))
-        y_cordinate = int((window.winfo_screenheight() / 2) - (window.winfo_height() / 2))
+        x_cordinate: int = int((window.winfo_screenwidth() / 2) - (window.winfo_width() / 2))
+        y_cordinate: int = int((window.winfo_screenheight() / 2) - (window.winfo_height() / 2))
         window.geometry(f"+{x_cordinate}+{y_cordinate - 20}")
         window.resizable(False, False)
 
-    def create_widgets(self):
+    def create_widgets(self) -> None:
         # About button
         button_help = ttk.Button(self.root, text='About', command=self.open_about_window)
         button_help.grid(row=0, column=0, columnspan=2, pady=10, padx=10, sticky='nw')
 
         # Filetype to convert to
-        self.current_to = tk.StringVar(value=Filetypes.PNG.value)
-        options_to = sorted(option.value for option in Filetypes if option.value not in {"JPG", "HEIC", "SVG", "TIF"})
-        label_to = ttk.Label(self.root, text='File type to convert to:')
-        label_to.grid(row=2, column=0, pady=15, padx=10)
-        optionmenu_to = ttk.OptionMenu(self.root, self.current_to, self.current_to.get(), *options_to)
-        optionmenu_to.grid(row=2, column=1, padx=20)
+        self.target_filetype = tk.StringVar(value=Filetypes.PNG.value)
+        options = sorted(option.value for option in Filetypes if option.value not in {"JPG", "HEIC", "SVG", "TIF"})
+        label = ttk.Label(self.root, text='File type to convert to:')
+        label.grid(row=2, column=0, pady=15, padx=10)
+        optionmenu = ttk.OptionMenu(self.root, self.target_filetype, self.target_filetype.get(), *options)
+        optionmenu.grid(row=2, column=1, padx=20)
 
         # Convert button
-        self.converter = Converter(self.root, self.current_to.get())
+        self.converter = Converter(self.root, self.target_filetype.get())
         button_convert = ttk.Button(self.root, text='Choose files', command=self.convert_files)
         button_convert.grid(row=3, column=0, columnspan=2, pady=10)
 
-    def convert_files(self):
-        """
-        Startet den Konvertierungsprozess basierend auf der Auswahl.
-        """
-        self.converter.target = self.current_to.get()
+    def convert_files(self) -> None:
+        self.converter.target = self.target_filetype.get()
         self.converter.convert()
 
-
-    def open_about_window(self):
+    def open_about_window(self) -> None:
         about_window = tk.Toplevel(self.root)
         self.center_window(about_window, width_offset=100, height_offset=-35)
         about_window.title('About')
