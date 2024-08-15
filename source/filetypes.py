745879dae5cb5bbd90805abcbdd6ca7bb1a3d6e4
Julian Hoffmann <julian.hoffmann@outlook.com>
2024-08-15 16:15:14 +0200

    new folder structure

---
+++ source/filetypes.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class Filetypes(Enum):
+    HEIC = ('HEIC')
+    JPEG = ('JPEG')
+    JPG = ('JPG')
+    WEBP = ('WEBP')
+    BMP = ('BMP')
+    ICO = ('ICO')
+    SVG = ('SVG')
+    PNG = ('PNG')
+    TIF = ('TIF')
+    PDF = ('PDF')
